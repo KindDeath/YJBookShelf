98d9208ce2c020276cf8a9d7b6bd2bac8acf5c49
gongyj <yijie.gong@ichano.com>
2016-07-08 13:56:55 +0800

    init project

---
+++ YJBookShelf/YJBookShelf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJBookShelf
+//
+//  Created by GYJ on 16/7/8.
+//  Copyright © 2016年 GYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
