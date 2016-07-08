5ad99a7b2d9c8568b2381600af2c94a0533ff478
gongyj <yijie.gong@ichano.com>
2016-07-08 14:26:33 +0800

    添加文件

---
+++ Source/YJBookShelfCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  YJBookShelfCollectionView.h
+//  YJBookShelf
+//
+//  Created by GYJ on 16/7/8.
+//  Copyright © 2016年 GYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJBookShelfCollectionView : UICollectionView
+
+@end
---
+++ Source/YJBookShelfCollectionView.m
@@ -0,0 +1,21 @@
+//
+//  YJBookShelfCollectionView.m
+//  YJBookShelf
+//
+//  Created by GYJ on 16/7/8.
+//  Copyright © 2016年 GYJ. All rights reserved.
+//
+
+#import "YJBookShelfCollectionView.h"
+
+@implementation YJBookShelfCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
